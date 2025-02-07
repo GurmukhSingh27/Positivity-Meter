8335ba3faeef7fa8488a1ed1ef0755ea8609b618
Gurmukh Singh Dadiala <busygurmukh@gmail.com>
2025-02-07 11:49:50 +0530

    first commit

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUpLong } from "@fortawesome/free-solid-svg-icons";
+import { faDownLong } from "@fortawesome/free-solid-svg-icons";
+import "./Counter.css";
+
+const Counter = () => {
+  const [increment, setincrement] = useState(0);
+
+  const incrementHandler = () => {
+    setincrement(increment + 1);
+  };
+
+  const decrementHandler = () => {
+    setincrement(increment - 1);
+  };
+
+  return (
+    <div className="main">
+      <h1>POSITIVITY METER: {increment}</h1>
+      {increment >= 0 && increment < 9 ? (
+        <p>KEEP PRESSING</p>
+      ) : increment < 0 ? (
+        <p className="min-score">SHOULD NOT THINK NEGATIVE</p>
+      ) : increment >= 10 ? (
+        <p className="max-score">MAX SCORE REACHED</p>
+      ) : (
+        <p>KEEP PRESSING</p>
+      )}
+      <div className="btns">
+        <button
+          className="btn-green"
+          onClick={incrementHandler}
+          disabled={increment >= 10}
+        >
+          <FontAwesomeIcon icon={faUpLong} className="up" />
+        </button>
+        <button
+          className="btn-red"
+          onClick={decrementHandler}
+          disabled={increment <= -10}
+        >
+          <FontAwesomeIcon icon={faDownLong} className="up" />
+        </button>
+      </div>
+      {increment >= 10 ? (
+        <p className="limit">
+          * AMPLE OF POSITIVITY *<br></br>BE GROUNDED
+        </p>
+      ) : null}
+      {increment <= -10 ? (
+        <p className="limit">
+          !! TOO MUCH NEGATIVITY !!<br></br>START BECOMING POSITIVE
+        </p>
+      ) : null}
+    </div>
+  );
+};
+
+export default Counter;
